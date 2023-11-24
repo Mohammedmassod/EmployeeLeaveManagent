61a4e5783b8e313ef39d91de0b752f36841aa465
Mohammedmassod <mohammedmassowd@gmail.com>
2023-11-24 18:51:10 +0300

    showcurunt user

---
+++ Employee Leave Managent/Models/PermissionVM.cs
@@ -0,0 +1,11 @@
+﻿namespace Employee_Leave_Managent.Models
+{
+    public class PermissionVM
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
