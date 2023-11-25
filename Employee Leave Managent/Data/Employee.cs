a56062df381e91098a079f0025ff485b2ee4a7d6
Mohammedmassod <mohammedmassowd@gmail.com>
2023-11-25 03:56:48 +0300

    #pop

--- Employee Leave Managent/Data/Employee.cs
+++ Employee Leave Managent/Data/Employee.cs
@@ -1,17 +1,25 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace Employee_Leave_Managent.Data.Migrations
 {
     public class Employee : IdentityUser
     {
+        [Display(Name = "الاسم الاول")]
         public string Firstname { get; set; }
+        [Display(Name = "إسم العائلة")]
+
         public string Lastname { get; set; }
+        [Display(Name = "الرقم الوظيفي ")]
         public string TaxId { get; set; }
+        [Display(Name = "تاريخ الميلاد ")]
         public DateTime DateOfBirth { get; set; }
+        [Display(Name = "تاريخ التوظيف")]
         public DateTime DateJoined { get; set; }
     }
 }
